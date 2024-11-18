44ce8c2a2be9c1fd033e77130ee92bd11a52445a
rajiyashaik1984 <sulthana.sk.1984@gmail.com>
2024-11-18 15:50:06 +0000

    Full pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Razia-Hello World!'));
+app.get('/', (req, res) => res.send('Razia-Hello World!Added Manual Review stage to code Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
